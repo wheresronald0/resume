4b55e3235def2e6aaa58b1f00ccea220d45299cc
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-09-17 12:36:54 -0400

    added some info about me

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+const fs = require("fs");
+
+fs.readFile(__dirname + "/info.json", "utf8", function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    return data;
+  }
+});
+
+"bin": {
+    "your-command-here": "./index.js"
+  },
+  
